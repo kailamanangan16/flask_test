0dfae3fc735877d3ebc01fb6aab65b42315034c2
kaila <kaila041603@icloud.com>
2020-12-20 18:31:00 -0800

    buttons!!!

--- main.py
+++ main.py
@@ -1,23 +1,9 @@
 from flask import Flask, render_template, redirect, url_for, request, session, flash
 from datetime import timedelta
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import create_engine, inspect
+import playsound
 
 app = Flask(__name__)
 app.secret_key = "hello"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-db = SQLAlchemy(app)
-
-class users(db.Model):
-    _id = db.Column("id", db.Integer, primary_key=True)
-    name = db.Column("name", db.String(100))
-    email = db.Column("email", db.String(100))
-
-    def __init__(self, name, email):
-        self.name = name
-        self.email = email
 
 @app.route("/")
 def home():
@@ -64,5 +50,4 @@ def logout():
     return redirect(url_for("login"))
 
 if __name__ == "__main__":
-    db.create_all()
     app.run(debug=True, host='127.0.0.1',port='3000')
